c286ebb394cbe3ece0fe9da444e9e00c44b6bd7e
Michael Clemens // DK1MI <mclemens@noreply.codeberg.org>
2023-05-10 17:24:59 +0000

    „wt32pamon.ino“ ändern

--- wt32pamon.ino
+++ wt32pamon.ino
@@ -14,30 +14,34 @@
 // Debug Level from 0 to 4
 #define _ETHERNET_WEBSERVER_LOGLEVEL_      3
 
+#define FORMAT_SPIFFS_IF_FAILED true
+
+
 #include <WebServer_WT32_ETH01.h>
 #include "index.h"  // Main Web page header file
 #include "config.h"  // Config Web page header file
 #include <Preferences.h>
+#include "FS.h"
+#include "SPIFFS.h"
 
-Preferences translation_fwd;
-Preferences translation_ref;
 Preferences config;
 
-String config_items [ ] = {"show_mV", "show_dBm", "show_watt", "vswr_threshold"};
-String config_defaults [ ] = {"true", "true", "true", "2"};
+String config_items [ ] = {"show_mV", "show_dBm", "show_watt", "vswr_threshold", "selected_band"};
+String config_defaults [ ] = {"true", "true", "true", "2", "70cm"};
+double fwd_array [3300] = {};
+double ref_array [3300] = {};
 
 int voltage_fwd,voltage_ref;
-float fwd_dbm=0, ref_dbm=0;
+double fwd_dbm=0, ref_dbm=0;
 double fwd_watt=0, ref_watt=0;
 byte iii=0;
 
 String conf_content;
-String conf_translate_fwd_table = "";
-String conf_translate_ref_table = "";
+String conf_textareas = "";
 String conf_config_table = "";
-String del_action = "";
 
 String band = "70cm";
+String default_band = "70cm";
 String band_fwd = band + "_fwd";
 String band_ref = band + "_ref";
 String band_list []= {"1.25cm", "3cm", "6cm", "9cm", "13cm", "23cm", "70cm", "2m", "HF"};
@@ -55,28 +59,68 @@ WebServer server(80);
 // Google DNS Server IP
 //IPAddress myDNS(8, 8, 8, 8);
 
+String readFile(fs::FS &fs, const char * path){
+   Serial.printf("Reading file: %s\r\n", path);
+
+   File file = fs.open(path);
+   if(!file || file.isDirectory()){
+       Serial.println("− failed to open file for reading");
+       return "";
+   }
+  String ret = "";
+   Serial.println("− read from file:");
+   while(file.available()){
+      ret+=char(file.read());
+   }
+   file.close();
+   return ret;
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+   Serial.printf("Writing file: %s\r\n", path);
+
+   File file = fs.open(path, FILE_WRITE);
+   if(!file){
+      Serial.println("− failed to open file for writing");
+      return;
+   }
+   if(file.print(message)){
+      Serial.println("− file written");
+   }else {
+      Serial.println("− frite failed");
+   }
+   file.close();
+}
+
 
 // converts dBm to Watt
-double dbm_to_watt(float dbm) {
+double dbm_to_watt(double dbm) {
   return pow( 10.0, (dbm - 30.0) / 10.0);
 }
 
 // takes a voltage value and translates it
 // to dBm based on the corresponding lookup table
-float millivolt_to_dbm(int mv, bool fwd)
+double millivolt_to_dbm(int mv, bool fwd)
 {
-  float lastval = 0;
-  float nextval = 0;
+  double lastval = 0;
+  double nextval = 0;
   int lastkey = 0;
   int nextkey = 0;
-  float stored_val =0;
-  for (int i=0; i<3400; i++) {
+  double stored_val = 0;
+
+  for (int i=0; i<3300; i++) {
     if (fwd) {
-      stored_val = translation_fwd.getFloat(String(i).c_str());
+      //stored_val = translation_fwd.getFloat(String(i).c_str());
+      stored_val = fwd_array[i];
+      if (stored_val != 0){
+        Serial.println("key: " + String(i) + " - " + String(stored_val));
+      }
+      
     } else {
-      stored_val = translation_ref.getFloat(String(i).c_str());
+      //stored_val = translation_ref.getFloat(String(i).c_str());
+      stored_val = ref_array[i];
     }
-    if (!isnan(stored_val)) {
+    if (stored_val != 0) {
       if (i <  mv) {
         lastval = stored_val;
         lastkey = i;
@@ -87,24 +131,16 @@ float millivolt_to_dbm(int mv, bool fwd)
       }
     }
   }
+
+  Serial.println("lastkey: "+String(lastkey));
+  Serial.println("lastval: "+String(lastval));
  
-  float lowerkey = min(lastkey, nextkey);
-  float lowerval = min(lastval, nextval);
-   /*
-  float higherkey = max(lastkey, nextkey);
-  
-  float higherval = max(lastval, nextval);
-  float diffkey = higherkey - lowerkey;
-  float diffval = higherval - lowerval;
-  float x = diffval / diffkey;
-  float y = mv - lowerkey;
-  float z = x * y;
-  
-  float result = lowerval + z;
-  */
-  float diffkey = max(lastkey, nextkey) - min(lastkey, nextkey);
-  float diffval = max(lastval, nextval) - min(lastval, nextval);
-  float result = lowerval + ((diffval / diffkey) * (mv - lowerkey));
+  double lowerkey = min(lastkey, nextkey);
+  double lowerval = min(lastval, nextval);
+
+  double diffkey = max(lastkey, nextkey) - min(lastkey, nextkey);
+  double diffval = max(lastval, nextval) - min(lastval, nextval);
+  double result = lowerval + ((diffval / diffkey) * (mv - lowerkey));
 
   //Serial.print("measured voltage: " + String(mv) + "   LastVal: " + String(lastval) + "    LastKey: " + String(lastkey) + "   Nextval: " + String(nextval) + "   NextKey:" + String(nextkey) + "\n");
   return result;
@@ -117,13 +153,13 @@ void read_directional_couplers()
 { 
   int voltage_sum_fwd = 0;
   int voltage_sum_ref = 0;
-  for(iii=0; iii<20; iii++)                                     // Take 20 samples and save the highest value
+  for(iii=0; iii<50; iii++)                                     // Take 20 samples and save the highest value
   { voltage_sum_fwd += analogReadMilliVolts(IO2_FWD);
     voltage_sum_ref += analogReadMilliVolts(IO4_REF);
   }
 
-  voltage_fwd = voltage_sum_fwd/20;                                         // use peak voltage for processing
-  voltage_ref = voltage_sum_ref/20;
+  voltage_fwd = voltage_sum_fwd/50;                                         // use peak voltage for processing
+  voltage_ref = voltage_sum_ref/50;
 
   fwd_dbm = millivolt_to_dbm(voltage_fwd, true);
   ref_dbm = millivolt_to_dbm(voltage_ref, false);
@@ -162,13 +198,14 @@ void handleNotFound()
   server.send(404, F("text/plain"), message);
 }
 
-String watt_or_williwatt(float val){
+String watt_or_williwatt(double val){
   String ret = "0";
   if (val < 1){
     ret = String(val*1000,0) + "mW";
   } else {
     ret = String(val,3) + "W";
   }
+  ret.replace("nan", "---");
   return ret;
 }
 
@@ -178,13 +215,13 @@ String watt_or_williwatt(float val){
 void handleDATA() {
   read_directional_couplers();
   // calculate VSWR
-  float vswr = (1 + sqrt(ref_watt/fwd_watt)) / (1 - sqrt(ref_watt/fwd_watt));
+  double vswr = (1 + sqrt(ref_watt/fwd_watt)) / (1 - sqrt(ref_watt/fwd_watt));
   String vswr_str = "-1";
   if (vswr >= 1) {
     vswr_str = String(vswr);
   }
 
-  float rl = fwd_dbm - ref_dbm;
+  double rl = fwd_dbm - ref_dbm;
 
   // get vswr_threshold from general config
   String vswr_threshold = config.getString(String("vswr_threshold").c_str());
@@ -203,19 +240,25 @@ void handleDATA() {
     ref_dbm_str = String(ref_dbm,3) + "dBm";
   }
 
-  String output = watt_or_williwatt(fwd_watt) + ";" + fwd_dbm_str + ";" + voltage_fwd_str + ";" + watt_or_williwatt(ref_watt) + ";" + ref_dbm_str + ";" + voltage_ref_str + ";" + vswr_str + ";" + String(rl) + ";" + band + ";" + String(vswr_threshold);
+  fwd_dbm_str.replace("nan", "-- ");
+  ref_dbm_str.replace("nan", "-- ");
+  String rl_str = (String(rl));
+  rl_str.replace("nan", "-- ");
+
+
+  String output = watt_or_williwatt(fwd_watt) + ";" + fwd_dbm_str + ";" + voltage_fwd_str + ";" + watt_or_williwatt(ref_watt) + ";" + ref_dbm_str + ";" + voltage_ref_str + ";" + vswr_str + ";" + rl_str + ";" + band + ";" + String(vswr_threshold);
   server.send(200, "text/plane", output);
 }
 
 // main function for displaying the configuration page
 // invoked by the "configuration" button on the dashboard page
 void handleCONFIG() {
-  if (conf_translate_fwd_table == "") {
-    build_translate_table(true);
+
+
+  if (conf_textareas == "") {
+    build_textareas();
   }
-  if (conf_translate_ref_table == "") {
-    build_translate_table(false);
-  }
+  
   if (conf_config_table == "") {
     build_config_table();
   }
@@ -239,11 +282,7 @@ void handleCONFIG() {
   conf_content += "</select></form>";
   conf_content += "<p>";
   conf_content += "<h2>Translation Detector voltage /mV to RF-Power level /dBm</h3>";
-  conf_content += "<h3>FWD</h3>";
-  conf_content +=  conf_translate_fwd_table;
-  conf_content += "<p>";
-  conf_content += "<h3>REF</h3>";
-  conf_content +=  conf_translate_ref_table;
+  conf_content +=  conf_textareas;
   conf_content += "<p>";
   conf_content += "<h2>General Configuration Items</h3>";
   conf_content += "<p>";
@@ -253,45 +292,45 @@ void handleCONFIG() {
   server.send(200, "text/html", conf_content);
 }
 
+
 // generates the translation table for either the FWD or
 // REF values
-void build_translate_table(bool fwd) {
-  String tbl = "";
-  if (fwd) {
-    tbl = "<form action=\"/modttfwd\" method=\"POST\">";
-  } else {
-    tbl = "<form action=\"/modttref\" method=\"POST\">";
-  }
-  tbl += "<table class='styled-table'>";
-  tbl += "<thead><tr><td>millivolt (mV)</td><td>decibel-milliwatts (dBm)</td><td>Watt</td><td>Action</td></tr></thead>";
+void build_textareas() {
+  String fwd = readFile(SPIFFS, String("/" + band + "fwd.txt").c_str());
+  String ref = readFile(SPIFFS, String("/" + band + "ref.txt").c_str());
 
-  for (int i=0; i<3400; i++) {
-    float stored_val = 0;
-    if (fwd) {
-      stored_val = translation_fwd.getFloat(String(i).c_str());
-    } else {
-      stored_val = translation_ref.getFloat(String(i).c_str());
+  Serial.println("From disk: "+ fwd);
+
+  clear_fwd_ref_array();
+
+  save_string_to_array(fwd, fwd_array);
+  save_string_to_array(ref, ref_array);
+
+  String tbl = "<form action=\"/modtranslation\" method=\"POST\">";
+  tbl += "<table class='styled-table'>";
+  tbl += "<thead><tr><td>FWD (mV:dBm)</td><td>REF (mV:dBm)</td></tr></thead>";
+  tbl += "<tr><td>";
+  tbl += "<textarea id='fwd_textarea' name='fwd_textarea' rows='30' cols='25'>";
+  for (int i=0; i<sizeof fwd_array/sizeof fwd_array[0]; i++) {
+    if (fwd_array[i] != 0){
+      tbl += String(i) +":"+String(fwd_array[i],5) + "\n";
     }
-    if (!isnan(stored_val)) {
-      tbl += "<tr><td>";
-      tbl += String(i);
-      tbl += "</td><td>";
-      tbl += String(stored_val,3);
-      tbl += "</td><td>";
-      tbl += String(dbm_to_watt(stored_val),5);
-      tbl += "</td><td>";
-      tbl += "<button class='button' value='" + String(i) + "' name='delete' type='submit'>delete</button>";
-      tbl += "</td></tr>";   
-    } 
   }
-  tbl += "<tr><td><input name='volt' length=16></td><td><input name='dBm' length=16></td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><button class='button' type='submit'>add/edit</button></td></tr>";
-  tbl += "</table></form>"; 
-    if (fwd) {
-      conf_translate_fwd_table = tbl;
-    } else {
-      conf_translate_ref_table = tbl;
+  tbl += "</textarea>";
+  tbl += "</td><td>";
+  tbl += "<textarea id='ref_textarea' name='ref_textarea' rows='30' cols='25'>";
+  for (int i=0; i<sizeof ref_array/sizeof ref_array[0]; i++) {
+    if (ref_array[i] != 0){
+      tbl += String(i) +":"+String(ref_array[i],5) + "\n";
     }
+  }
+  tbl += "</textarea>";
+  tbl += "</td></tr></table>";
+  tbl += "<p><button class='button' value='save' name='save' type='submit'>save</button>";
+  tbl += "</form>";
+  conf_textareas = tbl;
 }
+
 
 // generates the table with generic configuration items
 void build_config_table() {
@@ -317,33 +356,81 @@ void build_config_table() {
 }
 
 // Handle request from the config page to change or add values
-// to the FWD value table for the selected band
-void handleMODTTFWD() {
-  String volt = server.arg("volt");
-  String dBm = server.arg("dBm");
-  del_action = server.arg("delete");
-  if (del_action != "") {
-    translation_fwd.remove(del_action.c_str());
-  } else if (volt != "" and dBm != "") {
-     translation_fwd.putFloat(volt.c_str(), dBm.toFloat());
+// to the XXXX value table for the selected band
+void handleMODTRANS() {
+  String fwd = server.arg("fwd_textarea") + "\n";
+  String ref = server.arg("ref_textarea") + "\n";
+  clear_fwd_ref_array();
+  save_string_to_array(fwd,fwd_array);
+  save_string_to_array(ref,ref_array);
+
+
+  // OWN FUNCTION
+  String fwd_of_array = "";
+  for (int i=0; i<sizeof fwd_array/sizeof fwd_array[0]; i++) {
+    if (fwd_array[i] != 0){
+      fwd_of_array += String(i) +":"+String(fwd_array[i],5) + "\n";
+    }
   }
-  build_translate_table(true);
+  writeFile(SPIFFS, String("/" + band + "fwd.txt").c_str(), fwd_of_array.c_str());
+
+
+  String ref_of_array = "";
+  for (int i=0; i<sizeof ref_array/sizeof ref_array[0]; i++) {
+    if (ref_array[i] != 0){
+      ref_of_array += String(i) +":"+String(ref_array[i],5) + "\n";
+    }
+  }
+  writeFile(SPIFFS, String("/" + band + "ref.txt").c_str(), ref_of_array.c_str());
+
+  build_textareas();
   handleCONFIG();
 }
 
-// Handle request from the config page to change or add values
-// to the REF value table for the selected band
-void handleMODTTREF() {
-  String volt = server.arg("volt");
-  String dBm = server.arg("dBm");
-  del_action = server.arg("delete");
-  if (del_action != "") {
-    translation_ref.remove(del_action.c_str());
-  } else if (volt != "" and dBm != "") {
-     translation_ref.putFloat(volt.c_str(), dBm.toFloat());
+
+void clear_fwd_ref_array(){
+  for (int x = 0; x < sizeof(fwd_array) / sizeof(fwd_array[0]); x++)
+  {
+    fwd_array[x] = 0;
+    ref_array[x] = 0;
   }
-  build_translate_table(false);
-  handleCONFIG();
+}
+
+void save_string_to_array(String table_data, double arr []){
+
+  int r=0,t=0;
+    
+  for(int i=0;i<table_data.length();i++)
+  {
+    if(table_data[i] == '\n' || i==table_data.length())
+    {
+      if (i-r > 1)
+      {
+        //tmp[t] = table_data.substring(r,i);
+        Serial.println("row: " + table_data.substring(r,i));
+        String row = table_data.substring(r,i);
+        t++;
+
+        int r2=0,t2=0;
+        for(int j=0;j<row.length();j++)
+        {
+          if(row[j] == ':' || row[j] == '\n')
+          {
+            if (j-r2 > 1)
+            {
+              int key = row.substring(r2,j).toInt();
+              double val = row.substring(j+1).toDouble();
+              arr[key] = val;
+              t2++;
+            }
+            r2 = (j+1);
+          }
+        }
+      }
+      r = (i+1);
+    }
+      
+  }
 }
 
 // Handle request from the config page to change or add values
@@ -371,23 +458,15 @@ void handleBAND() {
   band = server.arg("bands");
   band_fwd = band + "_fwd";
   band_ref = band + "_ref";
-  translation_fwd.end();
-  translation_ref.end();
-  translation_fwd.begin(band_fwd.c_str(), false);
-  translation_ref.begin(band_ref.c_str(), false);
-  build_translate_table(true);
-  build_translate_table(false);
+  Serial.println("baaand:" + band);
+  config.putString(String("selected_band").c_str(), band);
+  build_textareas();
   handleCONFIG();
 }
 
 // initialization routine
 void setup()
 {
-  analogReadResolution(12);
-  translation_fwd.begin(band_fwd.c_str(), false);
-  translation_ref.begin(band_ref.c_str(), false);
-  config.begin("config", false);
-
   // DELETEME: Cleanup from old ver
   config.remove(String("show_fwd").c_str());
   config.remove(String("show_ref").c_str());
@@ -410,8 +489,6 @@ void setup()
   ETH.begin(ETH_PHY_ADDR, ETH_PHY_POWER);
   // Static IP, leave without this line to get IP via DHCP
 
-
-
   //ETH.config(myIP, myGW, mySN, myDNS);
 
   WT32_ETH01_waitForConnect();
@@ -420,17 +497,38 @@ void setup()
   server.on(F("/"), handleRoot);
   server.on("/readDATA", handleDATA);
   server.on("/config", handleCONFIG);
-  server.on("/modttfwd", handleMODTTFWD);
-  server.on("/modttref", handleMODTTREF);
   server.on("/modcfg", handleMODCFG);
   server.on("/selectband", handleBAND);
+  server.on("/modtranslation", handleMODTRANS);
 
+
+  if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)){
+    Serial.println("SPIFFS Mount Failed");
+    return;
+   }
 
   server.onNotFound(handleNotFound);
   server.begin();
 
   Serial.print(F("HTTP EthernetWebServer is @ IP : "));
   Serial.println(ETH.localIP());
+
+  analogReadResolution(12);
+  //translation_fwd.begin(band_fwd.c_str(), false);
+  //translation_ref.begin(band_ref.c_str(), false);
+  config.begin("config", false);
+
+  band = config.getString(String("selected_band").c_str());
+  if (band = ""){
+    config.putString(String("selected_band").c_str(), default_band);
+    band = default_band;
+  }
+
+  Serial.println("band: " + band);
+  build_textareas();
+  Serial.println("done");
+
+
 }
 
 void loop()
